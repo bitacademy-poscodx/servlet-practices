65cb3ae3829fd14efaa3f364965ed39ddb2df5d4
포스코DX 청년 IT 전문가 8기 <167659749+bitacademy-poscodx@users.noreply.github.com>
2024-05-23 13:20:35 +0900

    Add files via upload

--- guestbook2/src/main/java/guestbook/conroller/GuestbookServlet.java
+++ guestbook2/src/main/java/guestbook/conroller/GuestbookServlet.java
@@ -1,32 +1,58 @@
-package guestbook.conroller;
+package guestbook.controller;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import guestbook.dao.GuestbookDao;
+import guestbook.vo.GuestbookVo;
+
 public class GuestbookServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
+		request.setCharacterEncoding("utf-8");
+		
 		String action = request.getParameter("a");
-		if("".equals(action)) {
+		
+		if("deleteform".equals(action)) {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/deleteform.jsp");
+			rd.forward(request, response);
+		} else if("delete".equals(action)) {
+			String no = request.getParameter("no");
+			String password = request.getParameter("password");
 			
-		} else if("".equals(action)) {
+			new GuestbookDao().deleteByNoAndPassword(Long.parseLong(no), password);
+
+			response.sendRedirect(request.getContextPath() + "/gb");
+		} else if("add".equals(action)) {
+			String name = request.getParameter("name");
+			String password = request.getParameter("password");
+			String message = request.getParameter("message");
 			
+			GuestbookVo vo = new GuestbookVo();
+			vo.setName(name);
+			vo.setPassword(password);
+			vo.setMessage(message);
+			
+			new GuestbookDao().insert(vo);
+			response.sendRedirect(request.getContextPath() + "/gb");
 		} else {
-			/* default action (list) */
-		}
+			List<GuestbookVo> list = new GuestbookDao().findAll();
+			
+			request.setAttribute("list", list);
+			
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
+			rd.forward(request, response);
+		}		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
 }
