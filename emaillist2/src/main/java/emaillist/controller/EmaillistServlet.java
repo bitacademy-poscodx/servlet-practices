7ef97853e6f14b5ebe1918af66a58c1bc12b1edd
bitacademy-poscodx <poscoict.bitacademy@gmail.com>
2024-05-23 10:52:12 +0900

    emaillist2 #finish

--- emaillist2/src/main/java/emaillist/controller/EmaillistServlet.java
+++ emaillist2/src/main/java/emaillist/controller/EmaillistServlet.java
@@ -1,13 +1,16 @@
 package emaillist.controller;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import emaillist.dao.EmaillistDao;
+import emaillist.vo.EmaillistVo;
 
 public class EmaillistServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -16,13 +19,27 @@ public class EmaillistServlet extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		
 		String action = request.getParameter("a");
-		if("".equals(action)) {
+		if("form".equals(action)) {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/form.jsp");
+			rd.forward(request, response);			
+		} else if("add".equals(action)) {
+			String firstName = request.getParameter("fn");
+			String lastName = request.getParameter("ln");
+			String email = request.getParameter("email");
 			
-		} else if("".equals(action)) {
-			
+			EmaillistVo vo = new EmaillistVo();
+			vo.setFirstName(firstName);
+			vo.setLastName(lastName);
+			vo.setEmail(email);
+
+			new EmaillistDao().insert(vo);
+			response.sendRedirect(request.getContextPath() + "/el");
 		} else {
-			/* default action ( list) */
-			new EmaillistDao().findAll();
+			/* default action (list) */
+			List<EmaillistVo> list = new EmaillistDao().findAll();
+			request.setAttribute("list", list);
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
+			rd.forward(request, response);			
 		}
 	}
 
